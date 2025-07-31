b86d4226d014fdc44bbeef4a3802d319bcd364a3
Roman <romansevetc@mail.ru>
2025-07-31 12:51:57 +0300

    add expenses page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174, // Фиксируем порт
-    strictPort: true, // Запрещаем автоматический выбор другого порта, если 5174 занят
-  }
+  // server: {
+  //   port: 5174, // Фиксируем порт
+  //   strictPort: true, // Запрещаем автоматический выбор другого порта, если 5174 занят
+  // }
 })
